0063b4fa3017480e21b51b19dcb92b96887a3587
AnVo90 <anvo092022@gmail.com>
2023-01-09 19:37:55 +0700

    230109-break
    jump collider & flip

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -10,21 +12,37 @@ public class PlayerMovement : MonoBehaviour
     public float jumpSpeed = 5f;
     Vector2 inputMovement;
     Rigidbody2D myBody;
+    CapsuleCollider2D myCollider;
+    SpriteRenderer mFlip;
+
     // Start is called before the first frame update
     void Start()
     {
-        myBody= GetComponent<Rigidbody2D>();
+        myBody = GetComponent<Rigidbody2D>();
+
+        myCollider = GetComponent<CapsuleCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Movement();
+        FlipSprite();
+    }
+
+    private void FlipSprite()
+    {
+        bool isPlayerHasHorizontalMovement = Mathf.Abs(myBody.velocity.x) > Mathf.Epsilon;
+        if (isPlayerHasHorizontalMovement)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myBody.velocity.x), 1f);
+        }
     }
 
     void OnMove(InputValue value)
     {
         inputMovement = value.Get<Vector2>();
+      
     }
     
     void Movement()
@@ -35,6 +53,12 @@ public class PlayerMovement : MonoBehaviour
     void OnJump(InputValue value)
     {
         //inputJump = value.Get<Vector2>();
+
+
+        if (!myCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            return;
+        }
         myBody.velocity += new Vector2(0f, jumpSpeed);
     }
 }
